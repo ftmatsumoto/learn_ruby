657cd26658adddbf146b1aac9975595739e53e5d
ftmatsumoto <felipetmatsumoto@yahoo.com.br>
2015-07-22 20:59:18 -0300

    solucao 05_silly_blocks

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,1 +1,11 @@
-silly_blocks.rb
+def reverser
+	yield.split.map { |word| word.reverse }.join(" ")
+end
+
+def adder(n = 1)
+	yield + n
+end
+
+def repeater(n = 1, &block)
+	n.times { block.call }
+end
